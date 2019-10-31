6e5838fdc81b67123ad43b2c8c1fe7f1ebf4e743
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-31 01:30:29 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(source_array)
+ i=0
+ new_array=[]
+ while i< source_array.length do
+   new_array << yield(source_array[i])
+   i+=1
+ end
+ new_array
+end
+
+
+def reduce(source_array, starting_point=0)
+  i=0
+  sum=starting_point
+  while i < source_array.length
+    yield sum,source_array[i]
+    sum+=source_array[i]
+    i+=1
+  end
+  sum
+end
