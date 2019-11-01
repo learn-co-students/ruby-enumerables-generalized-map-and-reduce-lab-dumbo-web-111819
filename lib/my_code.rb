9196810b9befd20af69441604d2fdad21c20c28e
essential-hacker-4489 <akpojotor@yahoo.com>
2019-11-01 20:55:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,10 +10,27 @@ def map(source_array)
 end
 
 
-def reduce(source_array, starting_point=0)
-  sum=starting_point
-  source_array.map do |item|
-    sum=yield(sum,item)
+#def reduce(source_array, starting_point=0)
+#  sum=starting_point
+#  source_array.map do |item|
+#    sum=yield(sum,item)
+#  end
+#  sum
+#end
+def reduce(source_array, starting_point=nil)
+  if starting_point==nil 
+    sum=source_array[0]
+    i=1
+  else
+    sum=starting_point
+    i=0
+  end 
+  while i<source_array.length do
+    sum=yield(sum,source_array[i])
+    i+=1
   end
   sum
+  #source_array.map do |item|
+  #  sum=yield(sum,item)
+  #end
 end
