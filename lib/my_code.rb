d3b3a737603e6455fefce9af28c42fac0a507b2b
Ben Miriello <benmiriello@gmail.com>
2019-11-12 01:37:02 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
 # Your Code Here
+
+# def map(input, &block)
+#   result = []
+#   input.each { |n| result << block.call(n) }
+#   result
+# end
+
+def map(input)
+  output = []
+  i = 0
+  while i < input.length
+    output.push(yield(input[i]))
+    i += 1
+  end
+  output
+end
+
+
+def reduce(input, init = nil)
+  if init
+    n = init
+    i = 0
+  else
+    n = input[0]
+    i = 1
+  end
+  while i < input.length
+    n = yield(n, input[i])
+    i += 1
+  end
+  n
+end
+
