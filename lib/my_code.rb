63c1de22e051fadce14cc3c99bac9586d0f59f49
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 03:24:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,29 @@
-# Your Code Here
-def my_own_map(array) 
-  new = []
-  yield(new)
-end 
+def map(source_array)
+  new_array = []
+  index = 0 
+  while index < source_array.length do 
+    new_array.push(yield(source_array[index]))
+    index += 1 
+  end
+  new_array
+end
+
+def reduce(source_array, sv = nil)
+  if sv 
+    sum = sv 
+    index = 0 
+  else 
+    sum = source_array[0]
+    index = 1 
+  end 
   
-my_own_map(somerray){
-  |eleme
+  while index < source_array.length do 
+    sum = yield(sum, source_array[index])
+    index += 1 
+  end 
+  sum
+end
+
+
+
+
