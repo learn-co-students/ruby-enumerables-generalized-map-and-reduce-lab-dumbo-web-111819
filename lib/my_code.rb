23b35a3ffb332e642ad053f52b18b3acde2a4b90
Ajani Queeley <ajaniqueeley@gmail.com>
2019-11-15 16:44:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(arg)
+  new = []
+  i = 0
+  while i < arg.length
+    new.push(yield(arg[i]))
+    i += 1
+  end
+return new
+end
+
+def reduce(arg, startp = nil)
+  if startp
+    total = startp
+    i = 0
+  else
+    total = arg[0]
+    i = 1
+  end
+  while i < arg.length
+    total = yield(total, arg[i])
+    i += 1
+  end
+return total
+end
